5d6169eb4cd3ba3f105847638a18ac0a59706a00
atharvvv <73079283+atharvvv@users.noreply.github.com>
2021-05-07 16:49:36 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,10 @@
+list1=[12,-7,3,4,2,-12,-10]
+list2=[1,2,4,3,-1,-8]
+
+for i in list1:
+    if i>=0:
+        print(i, end=" " )
+
+for num in list2:
+    if num>=0:
+        print( \n num,  end=" ")
